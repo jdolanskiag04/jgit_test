38b1817837c8a87d609cd2a8fee11349cf73ff71
admin <mrjdolanski@gmail.com>
2020-09-29 14:32:15 +0200

    lkjaskljfljkljk

--- XLRelease/folder 1/demo template.groovy
+++ XLRelease/folder 1/demo template.groovy
@@ -1,6 +1,6 @@
 // Exported from:        http://Jans-MBP:5516/#/templates/Foldera827e0c42b334156929c8b5737e83a80-Release79f2070bfa864cb7befee03ff3225a12/releasefile
 // Release version:      9.7.0-SNAPSHOT
-// Date created:         Tue Sep 29 11:16:05 CEST 2020
+// Date created:         Tue Sep 29 14:32:15 CEST 2020
 
 xlr {
   template('demo template') {
@@ -13,12 +13,10 @@ xlr {
             
           }
           manual('b') {
-            description 'abcbacb'
-            attachments {
-              attachment("sql:Attachment4347099ad22042bf97f95f9efe9ec2e4") {
-                filename "attachments/Attachment4347099ad22042bf97f95f9efe9ec2e4-Screenshot 2020-09-16 at 13.05.07.png"
-              }
-            }
+            
+          }
+          manual('c') {
+            
           }
         }
       }
